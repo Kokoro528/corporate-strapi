bbbc42b344d3c59ab37b4d42288529ddaab70df2
Yinj Chen <chenyq@tongyuan.cc>
2022-08-07 09:14:51 +0800

    api token salt

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -2,4 +2,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '8618813f55f41944c3977e1a5cbd9518'),
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT', '927a043df3418770473dbfa09c378075')
+  }
 });
